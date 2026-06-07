b9f06c4e92e3a15740f3abe90d56cf7b429bc5ea
MohammadHosseinV <mohammadhosseinvalyzadeh@gmail.com>
2026-06-07 08:21:45 -0700

    Add files via upload

---
+++ customer_analysis/Customer Analysis.sql
@@ -0,0 +1,21 @@
+﻿--Number of orders per customer
+SELECT
+    CustomerID,
+    COUNT(*) AS TotalOrders
+FROM Sales.SalesOrderHeader
+GROUP BY CustomerID;
+
+--Top Customers
+SELECT Top 1 Percent
+    CustomerID,
+    SUM(TotalDue) AS TotalPurchase
+FROM Sales.SalesOrderHeader
+GROUP BY CustomerID
+ORDER BY TotalPurchase DESC;
+
+--مشتریانی با یک سفارش
+SELECT
+    CustomerID
+FROM Sales.SalesOrderHeader
+GROUP BY CustomerID
+HAVING COUNT(*) = 1;
